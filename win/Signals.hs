fb8da76947e99c6578b570a89d63f558abc8b05d
Sven Mattsen <sven@sven.cc>
2016-11-24 09:29:55 +0100

    windows signal handling

--- win/Signals.hs
+++ win/Signals.hs
@@ -1,4 +1,20 @@
 module Signals(installAbortSignal) where
 
+import Foreign.C.Types
+import Foreign
+
+sigInt = 2
+sigTerm = 15
+
+foreign import ccall "wrapper"
+  wrapper :: (CInt -> IO ()) -> IO (FunPtr (CInt -> IO ()))
+
+foreign import ccall safe "signal.h signal"
+  signal :: CInt -> FunPtr (CInt -> IO ()) -> IO CInt
+
 installAbortSignal :: IO () -> IO ()
-installAbortSignal io = return ()
+installAbortSignal io = do
+  fptr <- wrapper $ \_ -> io
+  signal sigInt fptr
+  signal sigTerm fptr
+  return ()
