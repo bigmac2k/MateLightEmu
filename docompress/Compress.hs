3a503baba0dd7e2f43f40ebb2cb00045ef41cf9f
Sven Mattsen <sven@sven.cc>
2017-01-04 14:30:23 +0100

    to version 0.1 of lz4-conduit

--- docompress/Compress.hs
+++ docompress/Compress.hs
@@ -5,4 +5,4 @@ import Control.Monad.Trans.Resource
 import qualified Data.Conduit.LZ4 as L
 
 compress :: MonadResource m => Conduit ByteString m ByteString
-compress = L.compress
+compress = L.compress Nothing
