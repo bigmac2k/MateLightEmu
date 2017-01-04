760b81f773e348ec99740870e03d23bfce79b3e9
Sven Mattsen <sven@sven.cc>
2017-01-04 14:18:44 +0100

    switched compression to lz4-conduit (experimental)

--- docompress/Compress.hs
+++ docompress/Compress.hs
@@ -2,7 +2,7 @@ module Compress (compress) where
 import Data.Conduit
 import Data.ByteString
 import Control.Monad.Trans.Resource
-import qualified Data.Conduit.Lzma(compress)
+import qualified Data.Conduit.LZ4 as L
 
 compress :: MonadResource m => Conduit ByteString m ByteString
-compress = Data.Conduit.Lzma.compress Nothing
+compress = L.compress
